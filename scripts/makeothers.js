449ceb391a68783c950a949b00de61f0dd0cfb8f
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-02-23 22:30:23 -0700

    Update makeothers.js

--- scripts/makeothers.js
+++ scripts/makeothers.js
@@ -8,27 +8,36 @@ const path = require("path");
 const glob = require("glob");
 const sharp = require("sharp");
 
-glob.glob(path.join(__dirname, "../emojis/png") + `/**/*.png`).then(async (relPaths) => {
-    // avif folder
-    const folders = [
-        { path: "avif" },
-        { path: "thumb/128x128", scale: 128 },
-        { path: "thumb/64x64", scale: 64 },
-        { path: "thumb/48x48", scale: 48 },
-        { path: "thumb/32x32", scale: 32 },
-    ];
-    for (const relPngPath of relPaths) {
-        const newFileName = `${path.basename(relPngPath).replace(".png", "")}.avif`;
+fs.readdir(path.join(__dirname, "../emojis"), (err, files) => {
+    if (err) throw err;
 
-        const image = fs.readFileSync(path.resolve(relPngPath));
-        for (const folder of folders) {
-            const newPath = path.join(path.join(__dirname, "../emojis/", folder.path), newFileName);
-            console.log("making", folder.path, newFileName);
-            if (!folder.scale) {
-                await sharp(image).avif().toFile(newPath);
-            } else {
-                await sharp(image).resize({ width: folder.scale, height: folder.scale, kernel: "cubic" }).avif().toFile(newPath);
+    for (const folder of files) {
+        if (folder.includes(".")) continue;
+
+        const folderPath = path.join(__dirname, "../emojis/", folder);
+        glob.glob(path.join(folderPath, "/png") + `/**/*.png`).then(async (relPaths) => {
+            // avif folder
+            const folders = [
+                { path: "avif" },
+                { path: "thumb/128x128", scale: 128 },
+                { path: "thumb/64x64", scale: 64 },
+                { path: "thumb/48x48", scale: 48 },
+                { path: "thumb/32x32", scale: 32 },
+            ];
+            for (const relPngPath of relPaths) {
+                const newFileName = `${path.basename(relPngPath).replace(".png", "")}.avif`;
+
+                const image = fs.readFileSync(path.resolve(relPngPath));
+                for (const folder of folders) {
+                    const newPath = path.join(path.join(folderPath, folder.path), newFileName);
+                    console.log("making", folder.path, newFileName);
+                    if (!folder.scale) {
+                        await sharp(image).avif().toFile(newPath);
+                    } else {
+                        await sharp(image).resize({ width: folder.scale, height: folder.scale, kernel: "cubic" }).avif().toFile(newPath);
+                    }
+                }
             }
-        }
+        });
     }
 });
